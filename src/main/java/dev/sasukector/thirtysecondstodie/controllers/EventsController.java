ceb45710a1a9c741e31932dfaa180be4eee32a24
Héctor Rodríguez <hd.rdz98@gmail.com>
2021-10-08 12:10:40 -0500

    Registered RARE events and created: EFFECT_RARE, BUNNIES_RARE, ILLUSIONER_RARE, SHUFFLE_RARE, HUNGER_RARE, CREEPER_RARE

--- src/main/java/dev/sasukector/thirtysecondstodie/controllers/EventsController.java
+++ src/main/java/dev/sasukector/thirtysecondstodie/controllers/EventsController.java
@@ -9,9 +9,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Zombie;
+import org.bukkit.entity.*;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -29,7 +27,10 @@ public class EventsController {
     public enum EventType {
         // Normal events
         MINI_ZOMBIES_NORMAL, ZOMBIE_NORMAL, SKELETON_NORMAL, CREEPER_NORMAL, SLIMES_NORMAL, ANVIL_NORMAL,
-        COBWEB_NORMAL, EFFECT_NORMAL, DROP_HANDED_NORMAL, SKIP_DAY_NORMAL, RANDOM_TP_NORMAL
+        COBWEB_NORMAL, EFFECT_NORMAL, DROP_HANDED_NORMAL, SKIP_DAY_NORMAL, RANDOM_TP_NORMAL,
+        // Rare events
+        EFFECT_RARE, BUNNIES_RARE, ILLUSIONER_RARE, SHUFFLE_RARE, HUNGER_RARE, CREEPER_RARE, NO_CHESTS_RARE,
+        VINDICATOR_RARE, FIRE_RARE, PUFFER_FISH_RARE, TRAP_RARE, SAND_RARE, LIQUIDS_RARE, DURABILITY_RARE
     }
 
     public static EventsController getInstance() {
@@ -58,6 +59,22 @@ public class EventsController {
         this.events.add(new Event(EventType.DROP_HANDED_NORMAL, "Mano de mantequilla"  , GameController.Category.NORMAL, 0));
         this.events.add(new Event(EventType.SKIP_DAY_NORMAL, "Adios día..."  , GameController.Category.NORMAL, 0));
         this.events.add(new Event(EventType.RANDOM_TP_NORMAL, "¿Dónde estoy?"  , GameController.Category.NORMAL, 0));
+
+        // RARE
+        this.events.add(new Event(EventType.EFFECT_RARE, "Enserio... no me siento bien", GameController.Category.RARE, 0));
+        this.events.add(new Event(EventType.BUNNIES_RARE, "¿Co-conter?", GameController.Category.RARE, 0));
+        this.events.add(new Event(EventType.ILLUSIONER_RARE, "¿Un mago?", GameController.Category.RARE, 0));
+        this.events.add(new Event(EventType.SHUFFLE_RARE, "¿Y mis cosas?", GameController.Category.RARE, 0));
+        this.events.add(new Event(EventType.HUNGER_RARE, "Tengo hambre...", GameController.Category.RARE, 0));
+        this.events.add(new Event(EventType.CREEPER_RARE, "¡EXPLOSIÓN!", GameController.Category.RARE, 0));
+        this.events.add(new Event(EventType.NO_CHESTS_RARE, "¿Y la llave?", GameController.Category.RARE, 60));
+        this.events.add(new Event(EventType.VINDICATOR_RARE, "Aldeano enojado", GameController.Category.RARE, 0));
+        this.events.add(new Event(EventType.FIRE_RARE, "Hace calor", GameController.Category.RARE, 30));
+        this.events.add(new Event(EventType.PUFFER_FISH_RARE, "Se ven esponjosos", GameController.Category.RARE, 30));
+        this.events.add(new Event(EventType.TRAP_RARE, "¿Sin salida?", GameController.Category.RARE, 0));
+        this.events.add(new Event(EventType.SAND_RARE, "¿Lag?", GameController.Category.RARE, 0));
+        this.events.add(new Event(EventType.LIQUIDS_RARE, "Juro que aquí había otra cosa", GameController.Category.RARE, 0));
+        this.events.add(new Event(EventType.DURABILITY_RARE, "Se gastan rápido", GameController.Category.RARE, 0));
     }
 
     public void handleNewEvent(Event event) {
@@ -154,6 +171,37 @@ public class EventsController {
                         break;
                     }
                 }
+            });
+            case EFFECT_RARE -> Bukkit.getOnlinePlayers().forEach(p -> {
+                switch (random.nextInt(3)) {
+                    case 0 -> p.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 600, 0));
+                    case 1 -> p.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 600, 0));
+                    default -> p.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 600, 0));
+                }
+            });
+            case BUNNIES_RARE -> Bukkit.getOnlinePlayers().forEach(p -> {
+                for (int i = 0; i < random.nextInt(3) + 3; ++i) {
+                    Rabbit rabbit = (Rabbit) p.getWorld().spawnEntity(p.getLocation(), EntityType.RABBIT);
+                    rabbit.setRabbitType(Rabbit.Type.THE_KILLER_BUNNY);
+                    rabbit.customName(Component.text("Conejito chocolatoso", TextColor.color(0x9E513B)));
+                }
+            });
+            case ILLUSIONER_RARE -> Bukkit.getOnlinePlayers().forEach(p -> p.getWorld().spawnEntity(p.getLocation(), EntityType.ILLUSIONER));
+            case SHUFFLE_RARE -> Bukkit.getOnlinePlayers().forEach(p -> {
+                ItemStack[] items = p.getInventory().getContents();
+                List<Integer> indexes = Stream.iterate(0, n -> n + 1).limit(items.length).collect(Collectors.toList());
+                Collections.shuffle(indexes);
+                for (int i = 0; i < items.length; ++i) {
+                    p.getInventory().setItem(i, items[indexes.get(0)]);
+                    indexes.remove(0);
+                }
+            });
+            case HUNGER_RARE -> Bukkit.getOnlinePlayers().forEach(p -> p.setFoodLevel(0));
+            case CREEPER_RARE -> Bukkit.getOnlinePlayers().forEach(p -> {
+                Creeper creeper = (Creeper) p.getWorld().spawnEntity(p.getLocation(), EntityType.CREEPER);
+                creeper.customName(Component.text("Creeper Atomico", TextColor.color(0x2A854A)));
+                creeper.setPowered(true);
+                Objects.requireNonNull(creeper.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED)).setBaseValue(0.4f);
             });
         }
     }
